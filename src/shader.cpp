67e1185c223503448e3a77669146545500180a13
Atsuyo_ <132666940+Atsuyo64@users.noreply.github.com>
2025-02-14 15:26:00 +0100

    chore: shader class

--- src/shader.cpp
+++ src/shader.cpp
@@ -2,23 +2,39 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
-static std::string& compile(char* path)
+static GLint compile(char *path, GLenum type)
 {
+    char const *string = nullptr;
     try
     {
         std::ifstream file;
         file.exceptions(std::ios::badbit | std::ios::failbit);
         file.open(path);
+        std::stringstream tmp;
+        tmp << file.rdbuf();
+        string = tmp.str().c_str();
     }
-    catch(const std::exception& e)
+    catch (const std::ifstream::failure &e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "ERROR::SHADER: Could not open file " << path << ": " << e.what() << std::endl;
+        return 0;
     }
-    
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &string, NULL);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (!status)
+    {
+        char msg[512];
+        GLint size;
+        glGetShaderInfoLog(shader,512,&size,msg);
+        std::cerr << "ERROR::SHADER: Compilation error of " << path << ": " << msg << std::endl;
+    }
+    return shader;
 }
 
 Shader::Shader(char *vertex_shader_path, char *fragment_shader_path)
 {
-
 }
